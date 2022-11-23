1508ad2a17e3c322f2e506ba51dbc8f86910cfbd
ployloyloy <ployloyloy06@gmail.com>
2022-11-23 15:31:06 +0700

    ploy up

--- frontend/src/components/ScreenOverlay/index.jsx
+++ frontend/src/components/ScreenOverlay/index.jsx
@@ -1,39 +1,42 @@
-import React from 'react'
-import NewGameButton from '../Status/NewGameButton'
+import React from "react";
+import NewGameButton from "../Status/NewGameButton";
+import "./overlay.css";
 
-const ScreenOverlay = ({ playerName, score, opponentScore, onClose, newGame }) => {
+const ScreenOverlay = ({
+  playerName,
+  score,
+  opponentScore,
+  onClose,
+  newGame,
+}) => {
+  const title =
+    score > opponentScore ? `${playerName} wins!` : `${playerName} loses!`;
 
-    const title = score > opponentScore ? `${playerName} wins!` : `${playerName} loses!`
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        position: "fixed",
+        backgroundColor: "rgba(0,0,0,0.5)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "4rem",
+        zIndex: 100,
+        color: "white",
+        backdropFilter: "blur(4px)",
+      }}
+    >
+      <div style={{ fontSize: 80 }}>{title}</div>
+      <div style={{ fontSize: 48 }}>{`${score} : ${opponentScore}`}</div>
+      <button type="button" className="continue-button" onClick={onClose}>
+        <span>Back To Board → </span>
+      </button>
+      <NewGameButton newGame={newGame} />
+    </div>
+  );
+};
 
-    return (
-        <div
-            style={{
-                height: '100vh',
-                width: '100vw',
-                position: 'fixed',
-                backgroundColor: 'rgba(0,0,0,0.5)',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '4rem',
-                zIndex: 100,
-                color: 'white',
-                backdropFilter: 'blur(4px)'
-            }}
-        >
-            <div style={{fontSize: 80}}>{title}</div>
-            <div style={{fontSize: 48}}>{`${score} : ${opponentScore}`}</div>
-            <button 
-                className='continue-button'
-                onClick={onClose}
-            >
-                continues →
-            </button>
-            <NewGameButton newGame={newGame} />
-        </div>
-    )
-
-}
-
-export default ScreenOverlay
+export default ScreenOverlay;
